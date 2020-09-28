033bf624d36aecbf00e48f0de2a3764e2b428add
DunderMifflinT3 <70239101+DunderMifflinT3@users.noreply.github.com>
2020-09-27 23:43:38 -0400

    Update Room.h
    Updated Header File

--- TextBasedAdventure/TextBasedAdventure/Room.h
+++ TextBasedAdventure/TextBasedAdventure/Room.h
@@ -7,62 +7,40 @@ using namespace std;
 #ifndef Room_H
 #define Room_H
 
+const int MAXADJACENTROOMS = 4;
+
 class Room
 {
 private:
 	int roomID;
-	string roomName;		
+	string roomName;
 	bool isPlayerInRoom;	//Checks if player is in room. May be useful for future functions
 	int numOfAdjacentRooms;	//Amount of rooms connected to this room. Used to interate for loops involving adjacent room array.
-
-	int adjacentRooms[];	//A list of rooms connected to this room
+	int adjacentRooms[MAXADJACENTROOMS];	//A list of rooms connected to this room
 
 public:
+	//Constructors
 
-	//Constructor
-	Room(int id, string name, int adj[], int numOfAdj)
+	Room()
 	{
-		setRoomID(id);
-		setRoomName(name);
-		setNumOfAdjacentRooms(numOfAdj);
-		setAdjacentRooms(adj, numOfAdj);
 	}
 
-	//Copy Constructor
-	Room(const Room &r)
+	Room(int id, string name, int adj[MAXADJACENTROOMS], int numOfAdj)
 	{
+		roomID = id;
+		roomName = name;
+		numOfAdjacentRooms = numOfAdj;
+		
+		for (int i = 0; i < MAXADJACENTROOMS; i++)
+		{
+			adjacentRooms[i] = adj[i];
+		}
 	}
 
 	//Destructor
 	~Room()
 	{
 	}
-
-
-	//Set Functions
-	void setRoomID(int id)
-	{
-		roomID = id;
-	}
-
-	void setRoomName(string name)
-	{
-		roomName = name;
-	}
-
-	void setNumOfAdjacentRooms(int num)
-	{
-		numOfAdjacentRooms = num;
-	}
-
-	void setAdjacentRooms(int adj[], int size)
-	{
-		for (int i = 0; i < size; i++)
-		{	
-			adjacentRooms[i] = adj[i];
-		}
-	}
-
 
 	//Get Functions
 	int getRoomID()
