c6f2df8ebe6fd5680113a323ca7bd99974f4bf1b
DunderMifflinT3 <70239101+DunderMifflinT3@users.noreply.github.com>
2020-10-25 18:37:33 -0400

    Update Source.cpp
    Updated version of the code pasted in. Has a lose scenario if it takes more then 10 attempts.

--- TextBasedAdventure/TextBasedAdventure/highlow/highlow/Source.cpp
+++ TextBasedAdventure/TextBasedAdventure/highlow/highlow/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 using namespace std;
 
 // HIGH LOW ROOM GAME
@@ -9,26 +10,35 @@ int main()
 	int randnumber;
 	int guess;
 	int tries = 0;
+	int attemptnumber = 10;
+	char playagain;
 
 	//generate random number from 1-100
 	srand(time(0));
 	randnumber = rand() % 100 + 1;
 
-	do
-	{
 		//reads first number
-		cout << "Enter a number from 1-100: ";
-		cin >> guess;
-		tries++;
+		cout << "You have 10 tries to guess the number from 1-100." << endl;
 
-		//gameplay statements
-		if (guess > randnumber)
-			cout << "Nope, too high." << endl;
-		else if (guess < randnumber)
-			cout << "Nope, too low." << endl;
-		else
-			cout << "Yes! That is the correct number. You got it in " << tries << " attempts.";
-	} while (guess != randnumber);
-
-	return false;
+		for (int i = 0; i < attemptnumber; i++)
+		{
+			cout << "Enter a number from 1-100:" << endl;
+			cin >> guess;
+			if (guess > randnumber)
+			{
+					cout << "Nope, too high." << endl;
+			}
+				else if (guess < randnumber) {
+				cout << "Nope, too low." << endl;
+					}
+				else {
+				cout << "Yes! That is the correct number. You got it in " << tries << " attempts.";
+				return true;
+				}
+				tries++;
+		}
+		{
+			cout << "You are out of guesses. Would you like to play again?";
+			return false;
+		}
 }
