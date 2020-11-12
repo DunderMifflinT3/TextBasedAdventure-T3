5306286a3f353d6b603098ae2bc9e65aec35259a
DunderMifflinT3 <70239101+DunderMifflinT3@users.noreply.github.com>
2020-11-12 17:02:08 -0500

    Update Source.cpp
    Can now be called in one line.

--- TextBasedAdventure/TextBasedAdventure/highlow/highlow/Source.cpp
+++ TextBasedAdventure/TextBasedAdventure/highlow/highlow/Source.cpp
@@ -4,8 +4,7 @@
 #include <string>
 using namespace std;
 
-// HIGH LOW ROOM GAME
-int main()
+bool playHL()
 {
 	int randnumber;
 	int guess;
@@ -17,28 +16,34 @@ int main()
 	srand(time(0));
 	randnumber = rand() % 100 + 1;
 
-		//reads first number
-		cout << "You have 10 tries to guess the number from 1-100." << endl;
+	//reads first number
+	cout << "You have 10 tries to guess the number from 1-100." << endl;
 
-		for (int i = 0; i < attemptnumber; i++)
+	for (int i = 0; i < attemptnumber; i++)
+	{
+		cout << "Enter a number from 1-100:" << endl;
+		cin >> guess;
+		if (guess > randnumber)
 		{
-			cout << "Enter a number from 1-100:" << endl;
-			cin >> guess;
-			if (guess > randnumber)
-			{
-					cout << "Nope, too high." << endl;
-			}
-				else if (guess < randnumber) {
-				cout << "Nope, too low." << endl;
-					}
-				else {
-				cout << "Yes! That is the correct number. You got it in " << tries << " attempts.";
-				return true;
-				}
-				tries++;
+			cout << "Nope, too high." << endl;
 		}
-		{
-			cout << "You are out of guesses. Would you like to play again?";
-			return false;
+		else if (guess < randnumber) {
+			cout << "Nope, too low." << endl;
 		}
+		else {
+			cout << "Yes! That is the correct number. You got it in " << tries << " attempts.";
+			return true;
+		}
+		tries++;
+	}
+	{
+		cout << "You are out of guesses.";
+		return false;
+	}
+}
+// HIGH LOW ROOM GAME
+int main()
+{
+	playHL();
+	return 0;
 }
