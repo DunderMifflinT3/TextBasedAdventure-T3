d184e2a8953e1ef15c1164b35eda15115352beb1
Michael LaNeve <mlaneve2000@gmail.com>
2020-11-14 14:04:47 -0500

    Input now works for everything. Only backspaces will crash the program.

--- TextBasedAdventure/TextBasedAdventure/InputValidation.h
+++ TextBasedAdventure/TextBasedAdventure/InputValidation.h
@@ -2,26 +2,41 @@
 
 #pragma once
 #include <iostream>
+#include <cctype>;
 #undef max
 using namespace std;
 
+bool checkIfDigit(string choice)
+{
+	for (int i = 0; i < choice.size(); i++)
+	{
+		if (!isdigit(choice[i]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
 bool inputValidation(string choice, int numOfChoices)	//Checks if input is valid
 {
-	if (stoi(choice) < 1 || stoi(choice) > numOfChoices)
+	if (choice.size() > 4)	//Prevents large inputs from causinng problems
+	{
+		cout << "Invalid Input" << endl;
+		return false;
+	}
+	else if(!checkIfDigit(choice))	//Makes sure that input is a digit
+	{
+		cout << "Invalid Input" << endl;
+		return false;
+	}
+	else if (stoi(choice) < 1 || stoi(choice) > numOfChoices)	//Makes sure input is within the specified range
 	{
 		cout << "Invalid Input" << endl;
 		return false;
 	}
 	else
 	{
-		for (int i = 0; i < choice.size(); i++)
-		{
-			if (!isdigit(choice[i]))
-			{
-				cout << "Invalid Input" << endl;
-				return false;
-			}
-		}
 		return true;
 	}
 }
@@ -32,7 +47,7 @@ int input(int numOfChoices)	//Allows for an input to happen
 
 	cout << endl;
 
-	do 
+	do //Only returns after the input is validated to be within 1 and the number of choices
 	{
 		getline(cin, inputString);
 
